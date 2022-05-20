966b8ac1a676db7be8cac6c1535dfb426f04bc5d
juanVansa <83036856+juanVansa@users.noreply.github.com>
2022-05-20 17:47:04 -0500

    web server creado

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 const express = require("express");
+const hbs = require("hbs");
+require("dotenv").config();
+
 console.clear();
-const port = 8080;
+const port = process.env.PORT;
 
 const app = express();
 
 //servir contenido estatico
 app.use(express.static("public"));
 
-//servir contenido estatico con un ruta definida
-app.get("/generic", (req, res) => {
-  res.sendFile(__dirname + "/public/generic.html");
+//HANDLEBARS
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+//servir contenido con handlebars enviando opciones
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Juan Diego",
+    titulo: "curso de node",
+  });
 });
+
 app.get("/elements", (req, res) => {
-  res.sendFile(__dirname + "/public/elements.html");
+  res.render("elements", {
+    nombre: "Juan Diego",
+    titulo: "curso de node",
+  });
 });
+
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    nombre: "Juan Diego",
+    titulo: "curso de node",
+  });
+});
+
+//servir contenido estatico con un ruta definida
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/public/404/index.html");
 });
 
+//puerto donde esta escuchando nuestro servidor
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
